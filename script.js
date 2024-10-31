6cc18da91947c96ff8b515ce771af33d74eb873d
Backa228 <andrey.scott01@gmail.com>
2024-10-31 11:30:37 +0200

    lesson31.10
    Доробили бокове меню.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menuToggle')
     const menuBody = document.getElementById('menuBody')
+    const overlay = document.getElementById('overlay')
 
     menuToggle.addEventListener('click', () => {
         menuBody.classList.toggle('active');
         menuToggle.classList.toggle('open');
+        overlay.classList.toggle('active');
     });
+
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 767.98) {
+            menuBody.classList.remove('active');
+            menuToggle.classList.remove('open');
+            menuBody.classList.add('no-transition')
+        } else {
+            menuBody.classList.remove('no-transition')
+        }
+    })
 });
+
