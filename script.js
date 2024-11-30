35089c567df330a914b18302b9c1b6183bb86acb
Backa228 <andrey.scott01@gmail.com>
2024-11-30 11:02:42 +0200

    lesson 30.11
    Доробили головну секцію, пофіксили існуючі баги.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menuToggle')
     const menuBody = document.getElementById('menuBody')
     const overlay = document.getElementById('overlay')
+    let lastScrollTop = 0;
+    const header = document.querySelector('.header');
 
     menuToggle.addEventListener('click', () => {
         menuBody.classList.toggle('active');
@@ -9,14 +11,36 @@ document.addEventListener('DOMContentLoaded', () => {
         overlay.classList.toggle('active');
     });
 
+    overlay.addEventListener('click', () => {
+        menuBody.classList.remove('active');
+        menuToggle.classList.remove('open');
+        overlay.classList.remove('active');
+    });
+
     window.addEventListener('resize', () => {
         if (window.innerWidth > 767.98) {
             menuBody.classList.remove('active');
             menuToggle.classList.remove('open');
+            overlay.classList.remove('active');
             menuBody.classList.add('no-transition')
         } else {
             menuBody.classList.remove('no-transition')
         }
     })
+
+    window.addEventListener ('scroll', () => {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+        if ((scrollTop > lastScrollTop) && (scrollTop > header.offsetHeight)) {
+            header.classList.add('');
+            header.classList.add('');
+        } else if (scrollTop < lastScrollTop) {
+            header.classList.remove('');
+        } 
+        if (scrollTop < header.offsetHeight) {
+            header.classList.remove('');
+        }
+
+    lastScrollTop = scrollTop;
+});
 });
 
